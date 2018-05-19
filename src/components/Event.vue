061379418acb142b401ad327ec6f16248c76474f
wonder95 <killshot91@gmail.com>
2018-05-18 17:43:58 -0700

    Obfuscate form.io URL in Event.vue.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -2,13 +2,14 @@
   <div class="section">
     <div class="container">
       <h1>Events</h1>
-    <!--  <formio src="https://nutmvrwoblsalot.form.io/event"></formio> -->
+        <formio src="formio_event_url"></formio>
     </div>
   </div>
 </template>
 
 <script>
 import { Formio } from 'vue-formio'
+
   export default {
     name: "Event",
     components: {
